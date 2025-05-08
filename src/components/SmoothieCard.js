6f9b9f20c6a1359d0d7f6858f10e036912029693
Kaushik Nag Tumu <138563218+404kaushik@users.noreply.github.com>
2025-05-08 06:52:43 -0400

    fetching data

---
+++ src/components/SmoothieCard.js
@@ -0,0 +1,12 @@
+const SmoothieCard = ({smoothie}) => {
+    return (
+        <div className="smoothie-card">
+            <h2>{smoothie.title}</h2>
+            <p>{smoothie.method}</p>
+            <div className="rating">{smoothie.rating}</div>
+        </div>
+    )
+
+}
+
+export default SmoothieCard;
